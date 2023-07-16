bf1e9389ef6d3c4dae760b355089c47ec77ea402
Alexander <mypost@example.com>
2023-07-16 16:42:43 +0300

    1 commit

---
+++ one_hot_vector.py
@@ -0,0 +1,18 @@
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+
+list_for_vector = list(set(lst))
+
+for i, o in data.iterrows():
+    # print(o['whoAmI'])
+    data.loc[i, 'one_hot'] = list_for_vector.index(o['whoAmI'])
+
+data['one_hot'] = data['one_hot'].astype('int')
+# data.drop(['whoAmI'], axis = 1, inplace = True)
+
+data
